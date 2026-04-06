605c5b54b82c93f85265af7804320e9e60870848
Guntur20 <guntur.subandi20@gmail.com>
2026-04-06 12:05:49 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/rt-dashboard-plus/", // <--- WAJIB TAMBAHKAN INI Sesuai Nama Repo Anda
   server: {
     host: "::",
     port: 8080,
